ef61c1040a77a5640c77b2589f666d3a0f8ceae0
Daniel Grieco <daniel_grieco@programmer.net>
2021-07-30 19:57:11 +0200

    Add class attributes for card suits, ranks and values in the Deck class

--- blackjack/deck.py
+++ blackjack/deck.py
@@ -9,6 +9,41 @@ class Deck:
     as well as access its methods.
     """
 
+    # All the card suits in the deck
+    card_suits = ("Clubs",
+                  "Diamonds",
+                  "Hearts",
+                  "Spades")
+    # All the card ranks in the deck
+    card_ranks = ("Two",
+                  "Three",
+                  "Four",
+                  "Five",
+                  "Six",
+                  "Seven",
+                  "Eight",
+                  "Nine",
+                  "Ten",
+                  "Jack",
+                  "Queen",
+                  "King",
+                  "Ace")
+    # All the card values based on ranks
+    # (Note that the dict keys must coincide with the values in the ranks tuple)
+    card_values = {"Two": 2,
+                   "Three": 3,
+                   "Four": 4,
+                   "Five": 5,
+                   "Six": 6,
+                   "Seven": 7,
+                   "Eight": 8,
+                   "Nine": 9,
+                   "Ten": 10,
+                   "Jack": 10,
+                   "Queen": 10,
+                   "King": 10,
+                   "Ace": 11}
+
     def __init__(self):
         """
         Initializes an instance of a Deck object.
