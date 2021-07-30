153a49daf77cc031fbf8218007bc1847bdb50702
Daniel Grieco <daniel_grieco@programmer.net>
2021-07-30 18:15:52 +0200

    Add deck module, containing the Deck class, to the blackjack package

---
+++ blackjack/deck.py
@@ -0,0 +1,15 @@
+"""
+This module holds the Deck class.
+"""
+
+
+class Deck:
+    """
+    This class allows you to instantiate a deck of cards
+    as well as access its methods.
+    """
+
+    def __init__(self):
+        """
+        Initializes an instance of a Deck object.
+        """
