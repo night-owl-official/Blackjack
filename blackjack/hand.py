58f3685c42ffe7b19da002486251afdbedd0fb7c
Daniel Grieco <daniel_grieco@programmer.net>
2021-08-02 19:39:32 +0200

    Create the hand.py module that holds the Hand class

---
+++ blackjack/hand.py
@@ -0,0 +1,17 @@
+"""
+This module holds the Hand class
+"""
+
+
+class Hand:
+    """
+    This class allows you to instantiate a hand of cards,
+    which is what the players and the dealer use to play blackjack.
+    """
+    
+    def __init__(self):
+        """
+        Initializes an instance of the Hand class.
+        """
+
+        pass
