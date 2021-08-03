52e72c1e5cefcf39fb960b121007713b22757031
Daniel Grieco <daniel_grieco@programmer.net>
2021-08-03 19:21:25 +0200

    Add fields and relative getters to the Currency class

--- blackjack/currency.py
+++ blackjack/currency.py
@@ -18,4 +18,27 @@ class Currency:
             when the game starts
         """
 
-        pass
+        self._total_tokens = initial_amount
+        self._tokens_bet = 0
+
+    @property
+    def total_tokens(self) -> int:
+        """
+        Gets the amount of tokens currently owned
+
+        Returns:
+             The total amount of held tokens
+        """
+
+        return self._total_tokens
+
+    @property
+    def tokens_bet(self) -> int:
+        """
+        Gets the amount of tokens the player bet
+
+        Returns:
+             The total amount of bet tokens
+        """
+
+        return self._tokens_bet
