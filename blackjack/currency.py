63d0ad4e964563455cca539805cc5a6ad2e9cdc9
Daniel Grieco <daniel_grieco@programmer.net>
2021-08-03 18:59:35 +0200

    Create the currency.py module alongside the Currency class

---
+++ blackjack/currency.py
@@ -0,0 +1,21 @@
+"""
+This module holds the Currency class
+"""
+
+
+class Currency:
+    """
+    This class allows you to represent a currency that the player
+    can use to bet as well as win or lose
+    """
+
+    def __init__(self, initial_amount: int):
+        """
+        Initializes an instance of Currency
+
+        Parameters:
+            initial_amount (int): The amount of the currency the player has
+            when the game starts
+        """
+
+        pass
