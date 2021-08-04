bc142271e2443abeca0934f7907a9d7634b96e72
Daniel Grieco <daniel_grieco@programmer.net>
2021-08-04 18:58:47 +0200

    Create blackjack.py module

---
+++ blackjack/blackjack.py
@@ -0,0 +1,8 @@
+"""
+This module contains all the functions necessary for the correct
+functioning of the blackjack game.
+"""
+from blackjack.card import Card
+from blackjack.deck import Deck
+from blackjack.hand import Hand
+from blackjack.currency import Currency
