84dc8f551cf9420c1c5310421afe61c7d6beee3e
Daniel Grieco <daniel_grieco@programmer.net>
2021-08-07 22:01:16 +0200

    Add a function to the blackjack module that asks the user whether they want to hit or stay amking sure their input is valid

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -2,6 +2,7 @@
 This module contains all the functions necessary for the correct
 functioning of the blackjack game.
 """
+from blackjack.card import Card
 from blackjack.deck import Deck
 from blackjack.hand import Hand
 from blackjack.currency import Currency
@@ -76,3 +77,55 @@ def ask_bet(currency: Currency):
         currency.bet(int(p_choice))
         print(f"You bet {currency.tokens_bet} tokens!\n")
         break
+
+
+def ask_hit_or_stay(hand: Hand, deck: Deck) -> str:
+    """
+    Asks the player whether they want to be hit or stay
+
+    Parameters:
+        hand (Hand): The player's hand
+        deck (Deck): The deck of cards
+
+    Returns:
+        The player's choice (hit or stay)
+    """
+
+    # This loop will run until the user enters a valid choice
+    while True:
+        # Asks the user if they want to hit or stay,
+        # showing them their hand to help decide
+        print("This is your hand:")
+        print(f"{hand}\n")
+        p_choice = input("Hit or Stay? (h/s) ").lower()
+
+        # The user wants to hit
+        # The choice is turned into lowercase so there's
+        # no need to check for abnormal versions of the word 'hit'
+        if (p_choice == 'h') or (p_choice == "hit"):
+            # The player is dealt a card from the deck
+            # The card object could be set to None just in case
+            # the deck has run out of cards
+            new_card = deck.deal_card()
+
+            if new_card:
+                # Adds the card to the player's hand
+                # and shows the user what card they drew
+                # as well as their new hand
+                # then returns the choice
+                hand.hit(new_card)
+                print(f"You got {new_card}!\n")
+                print("This is your hand:")
+                print(f"{hand}\n")
+                return 'h'
+            else:
+                print("* The deck has no more cards left! *\n")
+                break
+        elif (p_choice == 's') or (p_choice == "stay"):
+            # Prints a newline just for neater output
+            # and returns the choice
+            print()
+            return 's'
+
+        # The user entered some invalid input and is advised to enter the correct one
+        print("* You need to enter one of the following: h/hit, s/stay *\n")
