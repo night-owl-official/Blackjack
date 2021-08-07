6e3eda0d05a36ec38423c975d53f020f679829d1
Daniel Grieco <daniel_grieco@programmer.net>
2021-08-07 20:57:32 +0200

    Add a function to the blackjack module that allows to ask for a valid bet

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -48,3 +48,31 @@ def init_currency(starting_tokens=1000) -> Currency:
     """
 
     return Currency(starting_tokens)
+
+
+def ask_bet(currency: Currency):
+    """
+    Asks the player how much they want to bet
+
+    Parameters:
+        currency (Currency): The player's tokens to bet
+    """
+
+    # This loop will run until the user enters a number
+    while True:
+        # Asks the user to enter their bet, showing them
+        # the tokens they currently own
+        print(f"You have {currency.total_tokens} tokens left!")
+        p_choice = input("What's your bet? ")
+
+        # Checks the user's choice to make sure it's a number
+        # and if it's not then asks them to enter their bet again
+        if not p_choice.isdigit():
+            print("* You need to enter a number! *\n")
+            continue
+
+        # Places the bet as integer, displaying the bet to the console
+        # and ends the loop
+        currency.bet(int(p_choice))
+        print(f"You bet {currency.tokens_bet} tokens!\n")
+        break
