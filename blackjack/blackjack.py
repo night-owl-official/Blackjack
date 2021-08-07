b3b3d1762cf3f2a5a8561d94aa99a804d2801c82
Daniel Grieco <daniel_grieco@programmer.net>
2021-08-07 19:45:19 +0200

    Add function to the blackjack module to initialize the deck of cards and then return it

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -2,7 +2,21 @@
 This module contains all the functions necessary for the correct
 functioning of the blackjack game.
 """
-from blackjack.card import Card
 from blackjack.deck import Deck
 from blackjack.hand import Hand
 from blackjack.currency import Currency
+
+
+def init_deck() -> Deck:
+    """
+    Initializes the deck
+
+    Returns:
+        The deck of cards initialized and shuffled
+    """
+
+    # Initializes deck and shuffles it
+    d = Deck()
+    d.shuffle()
+
+    return d
